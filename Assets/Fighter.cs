51620c281d4a15c0071faa6585e83d16e8754845
aerinia <el2m299@gmail.com>
2024-12-05 14:42:10 +0300

    .

--- Assets/Fighter.cs
+++ Assets/Fighter.cs
@@ -6,39 +6,62 @@ public class Fighter : MonoBehaviour, IAction
 
     
 {
+    [SerializeField] float timeBetweenAttacks = 1f;
+    [SerializeField] float weaponDamage=10f;
     [SerializeField] float weaponRange;
     Transform targetObject;
+    float timeSinceLastAttack;
 
 
     private void Start()
     {
         weaponRange = 2.0f;
+        timeBetweenAttacks = 1f;
+        weaponDamage = 10f;
     }
     public void Attack(CombatTarget target)
     {
-        Debug.Log("Attack is done!");
+        GetComponent<ActionScheduler>().StartAction(this);
+        targetObject = target.transform;
     }
-
 
     private void Update()
     {
-        if (targetObject == null) { return; }
+        timeSinceLastAttack += Time.deltaTime;
+        if (targetObject == null) return;
 
-        bool isInRange= Vector3.Distance(transform.position,targetObject.position)< weaponRange;
-        if (isInRange==false) { GetComponent<Mover>().MoveTo(targetObject.position); }
-        else {
-            GetComponent<Animator>().SetTrigger("attack");
-            GetComponent<Mover>().Cancel(); 
+        bool isInRange = Vector3.Distance(transform.position, targetObject.position) < weaponRange;
+        if (!isInRange)
+        {
+            GetComponent<Mover>().MoveTo(targetObject.position);
+        }
+        else
+        {
+            GetComponent<Mover>().Cancel();
+            if (timeSinceLastAttack >= timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
         }
     }
 
-
-    public void Attack()
+    void Hit()
+    {
+        if (targetObject == null) return;
+        
+        Health health = targetObject.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(weaponDamage);
+        }
+    }
+ /*   public void Attack()
     {
         GetComponent<ActionScheduler>().StartAction(this);
         targetObject = targetObject.transform;
     }
-
+*/
     public void Cancel()
     {
         targetObject = null;
