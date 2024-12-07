720e768329b4151e33a6810a34ebb8a1774c9b51
aerinia <el2m299@gmail.com>
2024-12-08 01:18:53 +0300

    It cuts trees without problem

--- Assets/TreeCuttable.cs
+++ Assets/TreeCuttable.cs
@@ -1,54 +1,29 @@
 using UnityEngine;
-using System.Collections;
 
 public class TreeCuttable : MonoBehaviour
 {
-    [SerializeField] private int woodAmount = 3;
-    [SerializeField] private float timeToChop = 2f;
-    [SerializeField] private float regrowthTime = 10f;
-    [SerializeField] private GameObject woodPrefab;
-    
-    private bool isChopped = false;
-    
-    public float GetTimeToChop()
+    public int treeHealth = 3; // Number of hits before the tree is destroyed
+
+    // Call this method when the player starts chopping
+    public void StartChopping()
     {
-        return timeToChop;
-    }
-    
-    public bool CanBeChopped()
-    {
-        return !isChopped;
-    }
-    
-    public void ChopTree()
-    {
-        if (isChopped)
+        if (treeHealth > 0)
         {
-            Debug.Log($"Tree {gameObject.name} is already chopped!");
-            return;
+            treeHealth--;
+            Debug.Log("Tree health: " + treeHealth);
         }
-        
-        Debug.Log($"Chopping tree {gameObject.name}");
-        DropWood();
-        isChopped = true;
-        StartCoroutine(RegrowthTimer());
-    }
-    
-    private IEnumerator RegrowthTimer()
-    {
-        Debug.Log($"Tree {gameObject.name} will regrow in {regrowthTime} seconds");
-        yield return new WaitForSeconds(regrowthTime);
-        isChopped = false;
-        Debug.Log($"Tree {gameObject.name} has regrown!");
-    }
-    
-    private void DropWood()
-    {
-        Debug.Log($"Dropping {woodAmount} wood from tree {gameObject.name}");
-        for (int i = 0; i < woodAmount; i++)
+
+        if (treeHealth <= 0)
         {
-            Vector3 dropPosition = transform.position + Random.insideUnitSphere;
-            Instantiate(woodPrefab, dropPosition, Quaternion.identity);
+            ChopTree();
         }
     }
-} 
+
+    // This method is called when the tree is fully chopped
+    private void ChopTree()
+    {
+        // Optionally: Add logic to reward player with wood
+        Debug.Log("Tree chopped down!");
+        Destroy(gameObject); // Destroy the tree object
+    }
+}
